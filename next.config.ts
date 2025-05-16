3de498af1c1e442dc85042ca6d2322ae06679984
Schittenhelm <nic.schittenhelm@exxeta.com>
2025-05-16 13:59:38 +0200

    config build and deployment

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,21 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
+
 const nextConfig = {
   output: "export",
   trailingSlash: true,
-  basePath: "/team-next",
-  assetPrefix: "/team-next",
+  basePath: "/nicschittenhelm/team-next",
+  assetPrefix: "/nicschittenhelm/team-next/",
+
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 };
 
 export default nextConfig;
