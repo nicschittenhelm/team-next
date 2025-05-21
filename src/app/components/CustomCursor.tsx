1caae91654bb19ff396ae1e56fd19a13d24718e8
Schittenhelm <nic.schittenhelm@exxeta.com>
2025-05-21 14:26:17 +0200

    added custom cursor

---
+++ src/app/components/CustomCursor.tsx
@@ -0,0 +1,66 @@
+"use client";
+import React, { useEffect, useRef } from "react";
+import gsap from "gsap";
+
+export default function CustomCursor() {
+  const mouse = useRef({ x: 0, y: 0 });
+  const cursorPos = useRef({ x: 0, y: 0 });
+  const circle = useRef(null);
+  const size = 50;
+
+  // Lerp function for smooth interpolation
+  const lerp = (start, end, t) => start * (1 - t) + end * t;
+
+  const manageMouseMove = (e) => {
+    const { clientX, clientY } = e;
+
+    mouse.current = {
+      x: clientX,
+      y: clientY,
+    };
+  };
+
+  const moveCircle = (x, y) => {
+    gsap.set(circle.current, { x, y, xPercent: -50, yPercent: -50 });
+  };
+  useEffect(() => {
+    window.addEventListener("mousemove", manageMouseMove);
+
+    // Animation frame for smooth cursor movement using lerp
+    let animationFrameId;
+
+    const animate = () => {
+      // Apply lerp to both x and y coordinates (0.1 is the smoothing factor)
+      cursorPos.current.x = lerp(cursorPos.current.x, mouse.current.x, 0.1);
+      cursorPos.current.y = lerp(cursorPos.current.y, mouse.current.y, 0.1);
+
+      // Update the cursor position with interpolated values
+      moveCircle(cursorPos.current.x, cursorPos.current.y);
+
+      // Continue the animation loop
+      animationFrameId = requestAnimationFrame(animate);
+    };
+
+    // Start the animation loop
+    animate();
+
+    return () => {
+      window.removeEventListener("mousemove", manageMouseMove);
+      // Cleanup the animation frame when component unmounts
+      cancelAnimationFrame(animationFrameId);
+    };
+  }, []);
+  return (
+    <div className="fixed z-50 inset-0 h-screen pointer-events-none">
+      <div
+        ref={circle}
+        style={{
+          backgroundColor: "#BCE4F2",
+          width: size,
+          height: size,
+        }}
+        className="top-0 left-0 fixed rounded-full"
+      />
+    </div>
+  );
+}
